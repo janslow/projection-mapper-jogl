c3c04886ad0949a7445d14a7893ca354c20ed1c0
Jay Anslow <jay@anslow.me.uk>
2014-04-04 01:06:10 +0100

    Replaced AxisAngle4f with Rotation3f

--- src/com/jayanslow/projection/jogl/Application.java
+++ src/com/jayanslow/projection/jogl/Application.java
@@ -5,7 +5,6 @@ import java.awt.event.WindowEvent;
 import java.util.Collection;
 import java.util.LinkedList;
 
-import javax.vecmath.AxisAngle4f;
 import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
 
@@ -14,6 +13,7 @@ import com.jayanslow.projection.world.models.CuboidUniverse;
 import com.jayanslow.projection.world.models.FlatScreen;
 import com.jayanslow.projection.world.models.RealObject;
 import com.jayanslow.projection.world.models.RenderMode;
+import com.jayanslow.projection.world.models.Rotation3f;
 import com.jayanslow.projection.world.models.StandardProjector;
 import com.jayanslow.projection.world.models.Universe;
 
@@ -39,16 +39,16 @@ public class Application {
 		final Universe universe = new CuboidUniverse(new Vector3f(5000, 5000, 5000), objects, RenderMode.WIREFRAME);
 
 		int projector = 0;
-		objects.add(new StandardProjector(id++, projector++, new Vector3f(2500, 4000, 0),
-				new AxisAngle4f(0, -40, 50, 0), new Vector3f(300, 160, 450), 768, 1024, 3));
-		objects.add(new StandardProjector(id++, projector++, new Vector3f(0, 5000, 3000), new AxisAngle4f(20, -50, 20,
-				0), new Vector3f(300, 160, 450), 768, 1024, 1));
+		objects.add(new StandardProjector(id++, projector++, new Vector3f(2500, 4000, 0), new Rotation3f(
+				(float) -Math.PI / 6, 0, 0), new Vector3f(300, 160, 450), 768, 1024, 3));
+		objects.add(new StandardProjector(id++, projector++, new Vector3f(0, 5000, 3000), new Rotation3f(
+				(float) -Math.PI / 4, (float) -Math.PI / 4, 0), new Vector3f(300, 160, 450), 768, 1024, 1));
 
 		int screen = 0;
-		objects.add(new FlatScreen(id++, screen++, new Vector3f(500, 500, 5000), new AxisAngle4f(0, 0, -50, 0),
-				new Vector2f(4000, 4000)));
-		objects.add(new CuboidScreen(id++, screen++, new Vector3f(2000, 0, 3500), new AxisAngle4f(0, 0, -50, 0),
-				new Vector3f(1000, 1000, 1000)));
+		objects.add(new FlatScreen(id++, screen++, new Vector3f(500, 500, 5000), new Rotation3f(), new Vector2f(4000,
+				4000)));
+		objects.add(new CuboidScreen(id++, screen++, new Vector3f(2000, 0, 3500), new Rotation3f(0,
+				(float) Math.PI / 4, 0), new Vector3f(1000, 1000, 1000)));
 
 		return universe;
 	}
